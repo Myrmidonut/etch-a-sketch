e15ee1ffc451de0c3706e61d673c4b6a9ce5d5c0
Frederik Funke <frederik-funke@gmx.de>
2019-02-19 11:44:27 +0100

    favicon, readme images

--- README.md
+++ README.md
@@ -7,11 +7,12 @@ The main difference is the ability to jump to another spot on the drawing board 
 The app allows the user to create accounts and to save drawings to a database with customization options for a drawing like size, and color themes.
 Drawings of other users can be viewed, customized and saved to own accounts.
 
-## Preview Images
-![Drawing Board](drawingboard.jpg)
-![Gallery](gallery.jpg)
-![Settings](settings.jpg)
-![Account](account.jpg)
+***
+
+## Final Project
+https://fathomless-escarpment-99576.herokuapp.com/
+
+***
 
 ## Behind the scenes
 ### Frontend
@@ -24,3 +25,18 @@ Drawings of other users can be viewed, customized and saved to own accounts.
 ### Hosting
 * Heroku
 * ElephantSQL
+
+***
+
+## Preview Images
+### Drawing Board
+![Drawing Board](readme_images/drawingboard.jpg)
+
+### Gallery
+![Gallery](readme_images/gallery.jpg)
+
+### Settings
+![Settings](readme_images/settings.jpg)
+
+### Account
+![Account](readme_images/account.jpg)
--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -6,7 +6,15 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>Document</title>
+    <title>Etch-A-Sketch</title>
+
+    <link rel="apple-touch-icon" sizes="180x180" href="favicon/apple-touch-icon.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="favicon/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="favicon/favicon-16x16.png">
+    <link rel="manifest" href="favicon/site.webmanifest">
+    <link rel="mask-icon" href="favicon/safari-pinned-tab.svg" color="#5bbad5">
+    <meta name="msapplication-TileColor" content="#da532c">
+    <meta name="theme-color" content="#ffffff">
 
     <link href="https://fonts.googleapis.com/css?family=Share+Tech+Mono" rel="stylesheet">
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.6.3/css/all.css" integrity="sha384-UHRtZLI+pbxtHCWp1t77Bi1L4ZtiqrqD80Kn4Z8NTSRyMA2Fd33n5dQ8lWUE00s/" crossorigin="anonymous">
